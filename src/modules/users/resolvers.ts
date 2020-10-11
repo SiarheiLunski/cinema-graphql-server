abed8577922993ec67307bec6770b92bc525164a
Siarhei Lunski <loonskai@gmail.com>
2020-10-11 16:16:33 +0300

    Add non-optimized User query

--- src/modules/users/resolvers.ts
+++ src/modules/users/resolvers.ts
@@ -1,13 +1,23 @@
 import { Resolvers } from '../../types/schema';
 import { User } from '../../entity/User';
+import { Role } from '../../entity/Role';
 
 export const resolvers: Resolvers = {
   Query: {
     getUsers: async (_, args) => {
-      const users = await User.find({
+      const users = await User.find();
+      return users;
+    }
+  },
+  User: {
+    roles: async (parent) => {
+      console.log(parent);
+      const user = await User.findOne({
+        where: { id: parent.id },
         relations: ['roles']
       });
-      return users;
+      if (!user) return [];
+      return user.roles;
     }
   }
 };
